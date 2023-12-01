3a6df08fb6e60fa0c464e41319275266ba41a483
Balint Apro <balint.apro@falcon.io>
2023-12-01 23:19:10 +0100

    fix: optimise

--- src/01/index.ts
+++ src/01/index.ts
@@ -3,8 +3,8 @@ import * as fs from 'fs';
 const input = fs.readFileSync('input.txt', 'utf8');
 
 // utils
-const inputLines = () => input.split('\n')
-const sum = (numbers: any[]) => numbers.reduce((a: string, b: string) => parseInt(a) + parseInt(b))
+const inputLines = () => input.split('\n');
+const sum = (numbers: number[]) => numbers.reduce((a, b) => a + b);
 
 const dict: { [key: string]: number } = {
     one: 1,
@@ -19,21 +19,21 @@ const dict: { [key: string]: number } = {
 };
 
 // part one
-const extractDigits = (str: string): string => {
+const extractDigits = (str: string) => {
     const match = str.match(/\d/g) ?? '';
-    const first = match[0];
-    const last = match[match.length - 1];
-    return first.toString() + last.toString();
+    const first = parseInt(match[0]);
+    const last = parseInt(match[match.length - 1]);
+    return first * 10 + last;
 }
 
 console.info('First part: ', sum(inputLines().map(extractDigits)));
 
 // partTwo
-const extractNumber = (str: string): string => {
+const extractNumber = (str: string): number => {
     const match = str.match(/(\d|one|two|three|four|five|six|seven|eight|nine)/g) ?? '';
-    const first = dict[match[0]] ?? match[0];
-    const last = dict[match[match.length - 1]] ?? match[match.length - 1];
-    return first.toString() + last.toString();
+    const first = dict[match[0]] ?? parseInt(match[0]);
+    const last = dict[match[match.length - 1]] ?? parseInt(match[match.length - 1]);
+    return first * 10 + last;
 }
 
 console.info('Second part: ', sum(inputLines().map(extractNumber)));
