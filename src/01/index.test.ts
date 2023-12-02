ceb137a623d7238d0d85af63e07d15daebb21ccc
Balint Apro <balint.apro@falcon.io>
2023-12-02 01:46:40 +0100

    feat: tests, type fixes

---
+++ src/01/index.test.ts
@@ -0,0 +1,14 @@
+import { expect, test, describe } from "bun:test";
+import { input, firstPart, secondPart } from './index.ts'
+
+describe("checking day 1 solution", () => {
+
+    test('part 1 - should resolve to: 55386', () => {
+        expect(firstPart(input)).toBe(55386)
+    })
+
+    test('part 2 - should resolve to: 54807', () => {
+        expect(secondPart(input)).toBe(54807)
+    })
+
+});
--- src/01/index.ts
+++ src/01/index.ts
@@ -1,7 +1,8 @@
+import exp from 'constants';
 import fs from 'fs';
 
 // utils
-const lines = fs.readFileSync('input.txt', 'utf8').split('\n');
+export const lines = fs.readFileSync('input.txt', 'utf8').split('\n');
 const dict: { [key: string]: number } = {
     one: 1,
     two: 2,
@@ -17,21 +18,23 @@ const dict: { [key: string]: number } = {
 const sum = (numbers: number[]) => numbers.reduce((a, b) => a + b);
 
 // part one
-const digits = (str: string) => {
+const digits = (str: string): number => {
     const match = str.match(/\d/g) ?? '';
-    const first = parseInt(match[0]);
-    const last = parseInt(match[match.length - 1]);
-    return first * 10 + last;
+    const first = `${match[0]}`;
+    const last = `${match[match.length - 1]}`;
+    return parseInt(first + last);
 }
 
-console.info('First part: ', sum(lines.map(digits)));
+const firstPart = (lines: string[]) => sum(lines.map(digits));
 
 // part two
 const numbers = (str: string): number => {
     const match = str.match(/(\d|one|two|three|four|five|six|seven|eight|nine)/g) ?? '';
-    const first = dict[match[0]] ?? parseInt(match[0]);
-    const last = dict[match[match.length - 1]] ?? parseInt(match[match.length - 1]);
-    return first * 10 + last;
+    const first = `${dict[match[0]] ?? match[0]}`;
+    const last = `${dict[match[match.length - 1]] ?? match[match.length - 1]}`;
+    return parseInt(first + last);
 }
 
-console.info('Second part: ', sum(lines.map(numbers)));
+const secondPart = (lines: string[]) => sum(lines.map(numbers));
+
+export { firstPart, secondPart, lines as input };
