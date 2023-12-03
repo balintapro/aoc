2d9d5679faab0d12ec1c2fc1113b623c9103aa91
Balint Apro <balint.apro@falcon.io>
2023-12-03 21:20:05 +0100

    fix: only run one day

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import { partOne as d01p01, partTwo as d01p02, input as s01input } from './src/01/index.js';
-import { partOne as d02p01, partTwo as d02p02, input as s02input } from './src/02/index.js';
+// import { partOne as d01p01, partTwo as d01p02, input as s01input } from './src/01/index.js';
+// import { partOne as d02p01, partTwo as d02p02, input as s02input } from './src/02/index.js';
 import { partOne as d03p01, partTwo as d03p02, input as s03input } from './src/03/index.js';
 
-d01p01(s01input);
-d01p02(s01input);
-d02p01(s02input);
-d02p02(s02input);
+// d01p01(s01input);
+// d01p02(s01input);
+// d02p01(s02input);
+// d02p02(s02input);
 d03p01(s03input);
 d03p02(s03input);
